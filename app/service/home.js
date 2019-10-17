145adfbaf77f71ab34e4fe01d88d831dd9f89ccc
fengjutian <2640111081@qq.com>
2019-10-17 17:52:09 +0800

    静态资源

--- app/service/home.js
+++ app/service/home.js
@@ -3,9 +3,22 @@ const fs = require('fs');
 const Service = require('egg').Service;
 class HomeService extends Service {
   async getImgList() {
-    const fsImg = await fs.readFile('./public/assets/');
+    // let filesList = [];
+    // const imgUrl = '/assets/';
+    // let files = fs.readdirSync(imgUrl);
+    // files.forEach(function(itm, index) {
+    //   let stat = fs.statSync(imgUrl + itm);
+    //   let obj = {}; // 定义一个对象存放文件的路径和名字
+    //   obj.path = imgUrl; // 路径
+    //   obj.filename = itm; // 名字
+    //   filesList.push(obj);
+    // })
+
+    // return filesList;
+
+    // const fsImg = await fs.readFileSync('./public/assets/');
     // console.log(888, fsImg);
-    return fsImg;
+    return '333';
   }
 }
 
