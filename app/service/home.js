b44795ac4b05eaefdaaa3f2f7eecf3e9c580067d
fengjutian <2640111081@qq.com>
2019-10-24 14:20:42 +0800

    save

--- app/service/home.js
+++ app/service/home.js
@@ -5,15 +5,17 @@ class HomeService extends Service {
   async getImgList() {
     let filesList = [];
     const imgUrl = 'app/public/assets/';
+    const curImg = 'http://127.0.0.1:7002/' + 'static/assets/';
     let files = fs.readdirSync(imgUrl);
     files.forEach(function(itm, index) {
-      let stat = fs.statSync(imgUrl + itm);
+    // let stat = fs.statSync(imgUrl + itm);
       let obj = {}; // 定义一个对象存放文件的路径和名字
-      obj.path = imgUrl; // 路径
+      obj.path = curImg; // 路径
       obj.filename = itm; // 名字
+      obj.url = curImg + itm;
+    // obj.stat = stat;
       filesList.push(obj);
     })
-
     return filesList;
   }
 }
