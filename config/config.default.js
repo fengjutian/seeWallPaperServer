d5490627d2f5b114c3dddd4e5020631d0c8dd2f9
fengjutian <2640111081@qq.com>
2019-10-28 19:25:06 +0800

    cors

--- config/config.default.js
+++ config/config.default.js
@@ -34,6 +34,19 @@ module.exports = appInfo => {
     buffer: true, // in prod env, false in other envs
   };
 
+  config.middleware = [];
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true
+    },
+    domainWhiteList: ['http://127.0.0.1:7001']
+  };
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  };
+
   return {
     ...config,
     ...userConfig,
--- config/plugin.js
+++ config/plugin.js
@@ -6,4 +6,10 @@ module.exports = {
   // static: {
   //   enable: true,
   // }
+
+  cors: {
+    enable: true,
+    package: 'egg-cors'
+  }
 };
+
